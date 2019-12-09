54f6b420f4acb9fab8d87d642ec100989d4c29df
Maakep <schon.fredrik@gmail.com>
2019-12-09 20:07:08 +0100

    WIP

--- client/src/game/game.tsx
+++ client/src/game/game.tsx
@@ -1,65 +1,75 @@
-import * as React from 'react';
-import * as Socket from 'socket.io-client';
-import { CLIENT, SERVER } from '../../../common/events';
-import { Client, Question } from '../../../common/types';
+import * as React from "react";
+import * as Socket from "socket.io-client";
+import { CLIENT, SERVER } from "../../../common/events";
+import { Client, Question } from "../../../common/types";
 
 type Props = {
-    name: string;
-    room: string;
-}
+  name: string;
+  room: string;
+};
 
+type State = {
+  hasAnswered: boolean;
+  question: Question;
+};
 
-export default class extends React.PureComponent<Props, { question: Question }>{
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            question: undefined,
-        }
+export default class extends React.PureComponent<Props, State> {
+  socket: SocketIOClient.Socket;
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      question: undefined,
+      hasAnswered: false
+    };
 
-        const { name, room } = props;
-        const socket = Socket();
+    const { name, room } = props;
+    const socket = Socket();
+    this.socket = socket;
 
-        const client: Client = {
-            name,
-            room,
-        }
+    const client: Client = {
+      name,
+      room
+    };
 
-        socket.emit(CLIENT.JOIN, client);
+    socket.emit(CLIENT.JOIN, client);
 
-        socket.on(SERVER.NEW_QUESTION, (q: Question) => {
-            this.setState({ question: q });
-        });
+    socket.on(SERVER.NEW_QUESTION, (q: Question) => {
+      this.setState({ question: q, hasAnswered: false });
+    });
 
-        socket.on(SERVER.DONE, () => {
-            console.log("DONE, the game is over");
-        });
+    socket.on(SERVER.DONE, () => {
+      console.log("DONE, the game is over");
+    });
+  }
+
+  answer = (num: number) => {
+    if (this.state.question.correct == num) {
+      console.log("Correct answer");
+      this.socket.emit(CLIENT.ANSWER);
     }
+  };
 
-    answer = (num: number) => {
-        if (this.state.question.correct == num) {
-            console.log("Correct answer");
-        }
-    }
+  render = () => {
+    const { question, hasAnswered } = this.state;
+    const { room } = this.props;
 
-    render = () => {
-        const { question } = this.state;
-        const { room } = this.props;
-
-        return (
-            question ?
-                <div>
-                    {question.question}
-                    <ul>
-                        {question.answers.map((q, i) =>
-                            <li onClick={() => this.answer(i)} key={q}>{q}</li>
-                        )}
-                    </ul>
-                </div>
-                : <div>
-                    You have joined the room: {room}.
-                    Waiting for game to start.
-    </div >
-
-        )
-    }
+    return question ? (
+      hasAnswered ? (
+        <div>
+          {question.question}
+          <ul>
+            {question.answers.map((q, i) => (
+              <li onClick={() => this.answer(i)} key={q}>
+                {q}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div>You have joined the room: {room}. Waiting for game to start.</div>
+      )
+    ) : (
+      ""
+    );
+  };
 }
--- server/socket-events/monitor/index.ts
+++ server/socket-events/monitor/index.ts
@@ -3,34 +3,31 @@ import { Question } from "../../../common/types";
 import { rooms } from "../../room";
 
 const registerEventsForMonitor = (socket: SocketIO.Socket) => {
-    socket.on(MONITOR.JOIN, (roomId: string) => {
-        socket.join(roomId);
-        socket.emit(SERVER.ROOM, rooms[roomId]);
-    });
+  socket.on(MONITOR.JOIN, (roomId: string) => {
+    socket.join(roomId);
+    socket.emit(SERVER.ROOM, rooms[roomId]);
+  });
 
+  socket.on(MONITOR.START, (roomId: string) => {
+    const room = rooms[roomId];
+    const numOfQuestions = room.questions.length;
+    let i = 0;
 
-    socket.on(MONITOR.START, (roomId: string) => {
-        const room = rooms[roomId];
-        const numOfQuestions = room.questions.length;
-        let i = 0;
+    const nextQuestion = () => {
+      socket.to(roomId).emit(SERVER.NEW_QUESTION, room.questions[i]);
+      i++;
 
-        const nextQuestion = () => {
-            socket.to(roomId).emit(SERVER.NEW_QUESTION, room.questions[i]);
-            i++;
+      if (i == numOfQuestions) {
+        socket.to(roomId).emit(SERVER.DONE);
+      } else {
+        setTimeout(() => {
+          nextQuestion();
+        }, 30 * 1000);
+      }
+    };
 
-            if (i == numOfQuestions) {
-                socket.to(roomId).emit(SERVER.DONE);
-            } else {
-                setTimeout(() => {
-                    nextQuestion();
-                }, 30 * 1000)
-            }
-        }
-
-        nextQuestion();
-    });
-
-
-}
+    nextQuestion();
+  });
+};
 
 export default registerEventsForMonitor;
